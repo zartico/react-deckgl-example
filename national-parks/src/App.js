cf504dd60aae3f34aea69e08860daa32c545a78e
Jiri Zajic <jiri.zajic@zartico.com>
2023-05-22 09:41:35 +0200

    National Parks - Showing visitor data

--- national-parks/src/App.js
+++ national-parks/src/App.js
@@ -1,60 +1,80 @@
-import "./App.css";
-import { useState } from "react";
-import NATIONAL_PARKS_DATA from "./data.json";
+import React, { useState, useEffect } from "react";
 import { Map } from "react-map-gl";
-import DeckGL, { GeoJsonLayer } from "deck.gl";
+import DeckGL, { LineLayer } from "deck.gl";
+import { CSVLoader } from '@loaders.gl/csv';
+import { load } from '@loaders.gl/core';
+
+import "./App.css";
+
+// import visitorData from './data/keplertest_visitoronly.csv';
+import visitorData from './data/keplertest_visitoronly_smol.csv';
+// import visitorData from './data/keplertest_visitoronly_smolest.csv';
 
 const MAPBOX_ACCESS_TOKEN =
   "pk.eyJ1IjoiZGplcHBzIiwiYSI6ImNsZ3kzNW56YjAydDYzZm83dXhiYmg5bTkifQ.ffLjmgXnf73SWbSPtYjPCg";
-const MAP_STYLE =
-  "https://basemaps.cartocdn.com/gl/positron-gl-style/style.json";
-  // "https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json";
+const MAP_STYLE = "mapbox://styles/mapbox/dark-v9";
 
 const INITIAL_VIEW_STATE = {
-  latitude: 39.8283,
-  longitude: -98.5795,
-  zoom: 3,
+  longitude: -111.91,
+  latitude: 40.59,
+  zoom: 10.5,
   bearing: 0,
-  pitch: 30,
+  pitch: 30
 };
 
 function App() {
-  const [showPopup, setShowPopup] = useState(false);
+  const [data, setData] = useState([]);
 
-  const onClick = info => {
-    // alert(info.object.properties.Name)
-    // if (info.object) {
-    // } else {
-    // }
-  } 
+  useEffect(() => {
+    // https://devtrium.com/posts/async-functions-useeffect
+    const fetchData = async () => {
+      const csvData = await load(visitorData, CSVLoader);
+      setData(csvData);
+    };
 
-  const layers = [
-    new GeoJsonLayer({
-      id: 'nationalParks',
-      data: NATIONAL_PARKS_DATA, 
-      filled: true,
-      pointRadiusMinPixels: 5,
-      pointRadiusScale: 2000,
-      getPointRadius: f => 5,
-      getFillColor: data => data.properties.Name.includes("National Park") ? [198,128,73] : [60,95,55],
-      pickable: true,
-      autoHighlight: true
-    })
-  ]
+    fetchData();
+  }, []);
+
+  const lineLayer1 = new LineLayer({
+    id: 'line-layer1',
+    data,
+    getWidth: 1,
+    getSourcePosition: d =>
+      d.MovementStart.split(',').map(c => parseFloat(c.trim())),
+    getTargetPosition: d =>
+      d.MovementStop.split(',').map(c => parseFloat(c.trim())),
+    getColor: () => [0, 128, 255]
+  });
+
+  const lineLayer2 = new LineLayer({
+    // https://github.com/visgl/deck.gl/blob/8.9-release/docs/api-reference/layers/line-layer.md
+    id: 'line-layer2',
+    data: [{
+      inbound: 72633,
+      outbound: 74735,
+      from: {
+        name: '19th St. Oakland (19TH)',
+        coordinates: [-122.269029, 37.80787]
+      },
+      to: {
+        name: '12th St. Oakland City Center (12TH)',
+        coordinates: [-122.271604, 37.803664]
+      }
+    }],
+    getWidth: 50,
+    getSourcePosition: d => d.from.coordinates,
+    getTargetPosition: d => d.to.coordinates,
+    getColor: () => [0, 128, 255]
+  });
 
   return (
     <div>
-      <h1 className="App-header">US National Park System Locations</h1>
-      <h3 className="App-subheader">React/Deck.gl Example</h3>
-      <div className="key">        
-          <span className="keyColor light"></span> = National Park
-          <span className="keyColor dark"></span> = National Monument/Other        
-      </div>
-      <div className="mapContainer">
-        <DeckGL initialViewState={INITIAL_VIEW_STATE} controller={true} layers={layers} getTooltip={({object}) => object && (object.properties.Name)}>
-          <Map mapStyle={MAP_STYLE} mapboxAccessToken={MAPBOX_ACCESS_TOKEN} />
-        </DeckGL>
-      </div>
+      <DeckGL
+        initialViewState={INITIAL_VIEW_STATE}
+        controller={true}
+        layers={[lineLayer1, lineLayer2]}>
+        <Map mapStyle={MAP_STYLE} mapboxAccessToken={MAPBOX_ACCESS_TOKEN} />
+      </DeckGL>
     </div>
   );
 }
