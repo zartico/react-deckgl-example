bc781c1f80564ead49c6f219e917d95462d6e9ea
Jiri Zajic <jiri.zajic@zartico.com>
2023-05-25 14:50:51 -0600

    National Parks - CSV loader

--- national-parks/src/App.js
+++ national-parks/src/App.js
@@ -1,26 +1,18 @@
-import "./App.css";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 // import NATIONAL_PARKS_DATA from "./data.json";
 import { Map } from "react-map-gl";
 import DeckGL, { LineLayer } from "deck.gl";
 import { DataFilterExtension } from '@deck.gl/extensions';
+import { CSVLoader } from '@loaders.gl/csv';
+import { load } from '@loaders.gl/core';
 
 import visitorData from './data/keplertest_visitoronly_smol.csv';
 
-// const data = [
-//   {Visitor: 1, Income: 73274, MovementStart: '-112.017000571889, 40.65210387936112', MovementStop: '-112.01729694426133, 40.65236589944925', StartTime: '2022-09-25 02:15:52'},
-//   {Visitor: 1, Income: 73274, MovementStart: '-112.01707062329675, 40.65226654941521', MovementStop: '-112.01706792892287, 40.65233132595191', StartTime: '2022-09-25 16:12:07'},
-//   {Visitor: 1, Income: 73274, MovementStart: '-112.01706792892287, 40.65233132595191', MovementStop: '-112.01691165943448, 40.65239464536469', StartTime: '2022-09-25 16:12:44'},
-//   {Visitor: 1, Income: 73274, MovementStart: '-112.01691165943448, 40.65239464536469', MovementStop: '-112.01698979420834, 40.65236298568763', StartTime: '2022-09-25 16:14:20'},
-//   {Visitor: 1, Income: 73274, MovementStart: '-112.01698979420834, 40.65236298568763', MovementStop: '-112.01784119120741, 40.652209055272586', StartTime: '2022-09-25 16:23:52'},
-// ];
+import "./App.css";
 
 const MAPBOX_ACCESS_TOKEN =
   "pk.eyJ1IjoiZGplcHBzIiwiYSI6ImNsZ3kzNW56YjAydDYzZm83dXhiYmg5bTkifQ.ffLjmgXnf73SWbSPtYjPCg";
-const MAP_STYLE =
-     "mapbox://styles/mapbox/dark-v9";
-  // "https://basemaps.cartocdn.com/gl/positron-gl-style/style.json";
-  // "https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json";
+const MAP_STYLE = "mapbox://styles/mapbox/dark-v9";
 
 const INITIAL_VIEW_STATE = {
   width: window.innerWidth,
@@ -33,6 +25,7 @@ const INITIAL_VIEW_STATE = {
 }
 
 function App() {
+  const [data, setData] = useState([]);
   const [incomeMax, setIncomeMax] = useState(250000);
 
   const handleSliderChange = (e) => {
@@ -43,7 +36,7 @@ function App() {
   const layers = [
     new LineLayer({
       id: 'visitor-line',
-      data: visitorData,
+      data,
       getColor: [0, 128, 255],
       getSourcePosition: d => d.MovementStart.split(',').map(c => parseFloat(c.trim())),
       getTargetPosition: d => d.MovementStop.split(',').map(c => parseFloat(c.trim())),
@@ -55,9 +48,15 @@ function App() {
   ];
 
   useEffect(() => {
-    console.log(layers);
-  }, []);
+    // https://devtrium.com/posts/async-functions-useeffect
+    const fetchData = async () => {
+      const csvData = await load(visitorData, CSVLoader);
+      console.log(csvData);
+      setData(csvData);
+    };
 
+    fetchData();
+  }, []);
 
   return (
     <>
